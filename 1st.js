8fdd44e0acdf42f64fd9d2890e29d992121627e1
Muntasir <alimuntasir2001@gmail.com>
2026-04-17 20:49:24 +0600

    17-4-26

---
+++ 1st.js
@@ -0,0 +1,10 @@
+// const start = performance.now();
+
+console.time("Task");
+for (let i = 0; i<= 5000; i++)
+    console.log(i);
+
+console.timeEnd("Task");
+// const end = performance.now();
+
+// console.log(`This code took ${end - start} milliseconds to execute`);
